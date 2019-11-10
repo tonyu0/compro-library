913c59e79188ffbcdd8c7e5d5e6bd0662c58c13d
Tomoyasu Nakagawa <tonyu.grapefruit@gmail.com>
2019-11-11 00:12:39 +0900

    add ford-fulkerson

---
+++ flow/fordFulkerson.cpp
@@ -0,0 +1,95 @@
+// 説明: 貪欲法+逆辺による押し戻しにより最大流問題を解くアルゴリズム
+
+#include <iostream>
+#include <vector>
+#include <limits>
+using namespace std;
+
+struct Edge
+{
+    int dst, cap, rev;
+    Edge(int d, int c, int r) : dst(d), cap(c), rev(r) {}
+};
+
+using Edges = vector<Edge>;
+using Graph = vector<Edges>;
+
+// 有向グラフかどうかの情報も渡す
+template <typename T, bool directed>
+class FordFulkerson
+{
+public:
+    int n;       // 頂点数
+    Graph graph; // グラフ本体
+    vector<bool> used;
+
+    FordFulkerson(int n) : n(n), graph(n) {}
+    // 容量0の逆辺も張る
+    void add_edge(int src, int dst, T cap)
+    {
+        graph[src].emplace_back(dst, cap, graph[dst].size());
+        graph[dst].emplace_back(src, directed ? 0 : cap, graph[src].size() - 1);
+    }
+
+    T dfs(int v, int t, T flow)
+    {
+        if (v == t)
+            return flow;
+        used[v] = true;
+        for (auto &edge : graph[v])
+        {
+            if (!used[edge.dst] && edge.cap > 0)
+            {
+                T d = dfs(edge.dst, t, min(flow, edge.cap));
+                if (d > 0)
+                {
+                    // 流せるなら流す
+                    // 流した場合、辺の容量は減り、逆辺の容量は増える
+                    edge.cap -= d;
+                    graph[edge.dst][edge.rev].cap += d;
+                    return d;
+                }
+            }
+        }
+        return 0;
+    }
+
+    T max_flow(int s, int t)
+    {
+        T flow = 0;
+        while (true)
+        {
+            used.assign(n, false);
+            T f = dfs(s, t, numeric_limits<T>::max());
+            if (f == 0)
+                break;
+            flow += f;
+        }
+        return flow;
+    }
+};
+
+// https://atcoder.jp/contests/abc010/tasks/abc010_4
+void ABC010D()
+{
+    int N, G, E, p, a, b;
+    cin >> N >> G >> E;
+    FordFulkerson<int, false> ff(N + 1);
+    for (int i = 0; i < G; ++i)
+    {
+        cin >> p;
+        ff.add_edge(p, N, 1);
+    }
+    for (int i = 0; i < E; ++i)
+    {
+        cin >> a >> b;
+        ff.add_edge(a, b, 1);
+    }
+    cout << ff.max_flow(0, N) << endl;
+}
+
+int main()
+{
+    ABC010D();
+    return 0;
+}
