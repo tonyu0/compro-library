2ca6f8a66cfa584a378b930cf859a2722da06804
Tomoyasu Nakagawa <tonyu.grapefruit@gmail.com>
2019-11-16 15:08:30 +0900

    something

--- graph/warshallFloyd.cpp
+++ graph/warshallFloyd.cpp
@@ -1,9 +1,13 @@
-// 説明: 全点間最短距離, もう少し早くしたJohnsonというものもある。
-// TODO: https://atcoder.jp/contests/abc079/tasks/abc079_d  https://atcoder.jp/contests/abc143/tasks/abc143_e
+#include <iostream>
+#include <vector>
+using namespace std;
+// 説明: 全点間最短距離。
 
 // O(V^3)
-void warshall_floyd(int n, vector<vector<int> > &g) {
-  for(int k = 0; k < n; ++k)
-    for(int i = 0; i < n; ++i)
-      for(int j = 0; j < n; ++j) g[i][j] = min(g[i][j], g[i][k] + g[k][j]);
+void warshall_floyd(int n, vector<vector<int>> &g)
+{
+  for (int k = 0; k < n; ++k)
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < n; ++j)
+        g[i][j] = min(g[i][j], g[i][k] + g[k][j]);
 }
