0879f3e696a5a10a3ce93f427be6673ff2296cec
tonyu0 <tonyu.grapefruit@gmail.com>
2024-09-16 23:56:13 +0900

    avl tree

--- data-structure/avlTree.cpp
+++ data-structure/avlTree.cpp
@@ -1,12 +1,13 @@
 #pragma once
-// T keyは大小関係が定義できる必要がある。
-template <class T>
+// T keyは大小関係が定義できる必要がある vectorものる?
+
+template <typename T>
 class avl_tree {
 public:
   struct node {
     T key;
-    int size, height;
-    node *child[2];
+    int size, height; // subtree size / height
+    node *child[2];   // 0: small, 1: large
     node(const T &key) : key(key), size(1), height(1) {
       child[0] = child[1] = nullptr;
     }
@@ -26,10 +27,8 @@ public:
     node *res = rank(root, k);
     return res ? res->key : null;
   }
-  // TODO: order_of_keyを実装したい
-  // setを検索して、それが下から何番目か知りたくなる時がある。
-  // node *order_of_key(int x) const {}
-  // TODO: lower_bound, upper_boundを実装する
+  // T order_of_key(int x) const {} // TODO
+  // T lower_bound(int l, int r) const {} // TODO
 
 private:
   node *find(node *t, const T &key) {
@@ -42,11 +41,16 @@ private:
     }
     return t;
   }
-
+  // returns child after insert
   node *insert(node *t, node *x) {
     if (t == nullptr) { return x; } // insert x here
 
-    if (x->key <= t->key) {
+    if (x->key == t->key) {
+      // key duplicate
+      return x;
+    }
+
+    if (x->key < t->key) {
       t->child[0] = insert(t->child[0], x);
     } else {
       t->child[1] = insert(t->child[1], x);
@@ -54,7 +58,7 @@ private:
     t->size += 1;
     return balance(t);
   }
-
+  // returns child after erase
   node *erase(node *t, const T &x) {
     if (t == nullptr) { return nullptr; }
 
@@ -66,41 +70,49 @@ private:
     } else {
       t->child[1] = erase(t->child[1], x);
     }
-    t->size -= 1;
+    t->size -= 1; // can erase multiple values?
     return balance(t);
   }
-
+  // helper used from erase(),
+  //  t        .       l
+  // l r  ->  l r  ->   ..r (most descendant right of l)
   node *move_down(node *t, node *rhs) {
     if (t == nullptr) { return rhs; }
 
     t->child[1] = move_down(t->child[1], rhs);
     return balance(t);
   }
-
+  //   t      r
+  //  l r -> t
+  //    l'  l l' (an example of l<r case)
+  // (r to subtree root, if l<r, ccw rotate, if l>r, cw rotate)
   node *rotate(node *t, int l, int r) {
+    // in both of l<r and r>l cases, this rotation is valid. hard to explain
     node *s = t->child[r];
     t->child[r] = s->child[l];
     s->child[l] = balance(t);
-
-    _update_subtree_size(t);
-    _update_subtree_size(s);
+    update(t);
+    update(s);
     return balance(s);
   }
-
+  // returns subtree root of t after rotation
   node *balance(node *t) {
     for (int i = 0; i <= 1; ++i) {
-      if (_get_subtree_height(t->child[i ^ 1]) <
-          _get_subtree_height(t->child[i]) - 1) {
-        if (_get_subtree_height(t->child[i]->child[i ^ 1]) >
-            _get_subtree_height(t->child[i]->child[i])) {
-          t->child[i] = rotate(t->child[i], i, i ^ 1);
+      // balance factor(bf) := left height - right height
+      if (balance_factor(t->child[!i], t->child[i]) < -1) {
+        // if bf < -1 or bf > 1, then need rotate. in this case, it means i's
+        // subtree is too large. improve this situation by rotation.
+        if (balance_factor(t->child[i]->child[!i], t->child[i]->child[i]) > 0) {
+          // in this case, above bf cannot be positive(in short, it leads to be
+          // a bad rotation), then first rotate i's child.
+          t->child[i] = rotate(t->child[i], i, !i);
         }
-        return rotate(t, i ^ 1, i);
+        // need make i the root of t's subtree
+        return rotate(t, !i, i);
       }
     }
 
-    _update_subtree_height(t);
-    _update_subtree_size(t);
+    update(t);
     return t;
   }
 
@@ -116,32 +128,17 @@ private:
     return t; // this node is k-th smallest
   }
 
-  /* binary search
-   * 要素数を保持しておいて、木内でi番目の要素が
-   * 何なのかが分かるのを利用して二分探索をするとか。
-      int a = 目的の値
-      int st = -1, ed = tree.size(), mid;        //xxxooo, (st, ed]
-      while (ed - st >= 2) {
-          mid = (st + ed) / 2;
-          if ((tree.rank(mid))->key >= a) ed = mid;
-          else st = mid;
-      }
-      cout << sz - ed << endl;
-   */
-
   // helper
-  void _update_subtree_size(node *t) {
+  void update(node *t) {
     if (t != nullptr) {
       t->size = (t->child[0] ? t->child[0]->size : 0) +
                 (t->child[1] ? t->child[1]->size : 0) + 1;
-    }
-  }
-  void _update_subtree_height(node *t) {
-    if (t != nullptr) {
-      t->height = max((t->child[0] ? t->child[0]->height : 0),
-                      (t->child[1] ? t->child[1]->height : 0)) +
+      t->height = max(t->child[0] ? t->child[0]->height : 0,
+                      t->child[1] ? t->child[1]->height : 0) +
                   1;
     }
   }
-  int _get_subtree_height(node *t) { return (t ? t->height : 0); }
+  int balance_factor(node *l, node *r) {
+    return (l ? l->height : 0) - (r ? r->height : 0);
+  }
 };
