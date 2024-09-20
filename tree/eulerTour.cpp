72eddec699d6e5bb293ad8be4f89bc27b9d57ffa
tonyu0 <tonyu.grapefruit@gmail.com>
2024-09-20 23:38:30 +0900

    euler tour[WIP]

--- tree/eulerTour.cpp
+++ tree/eulerTour.cpp
@@ -1,27 +1,29 @@
-#include <iostream>
 #include <vector>
 using namespace std;
 
-// TODO: 木を列にしたものを返せればよい
-struct Edge {
-  int dst, cost;
-  Edge(int d, int c) : dst(d), cost(c) {}
-};
-
-using Edges = vector<Edge>;
-using Graph = vector<Edges>;
+// 木を列に置き換えて、区間に対する操作を可能にする。
+// パスに対して加算、部分木に対して加算、とか
+// 区間と実際の木の対応を考える必要がある
+// 基本的にコストは辺に乗っていることが多いので、辺でin/outたどったほうがわかりやすいかもしれない
+// 区間加算区間取得ができるとパスとか部分木に対して操作できる
 
 struct EulerTour {
   int N, cnt = 0;
-  Graph g;
-  vector<int> cost, rnk, ord;
+  vector<vector<pair<int, int>>> g;
+  vector<int> cost, in, out, ord;
   vector<pair<int, int>> depth;
 
-  EulerTour(int n, Graph &g) : g(g), rnk(n), cost(n), ord(n * 2) {
-    for (N = 1; N < n * 2; N <<= 1)
-      ;
+  EulerTour(int n) : g(n), in(n), out(n), cost(n), ord(n * 2) {
+    for (N = 1; N < n * 2; N <<= 1);
     depth.resize(N * 2 - 1);
+  }
 
+  void add_edge(int from, int to, int cost) {
+    g[from].push_back(make_pair(to, cost));
+    g[to].push_back(make_pair(from, cost));
+  }
+
+  void build() {
     dfs();
 
     for (int i = N - 2; i >= 0; --i)
@@ -29,19 +31,29 @@ struct EulerTour {
   }
 
   void dfs(int v = 0, int p = -1, int d = 0, int c = 0) {
-    rnk[v] = cnt, cost[v] = c;
-    depth[cnt + N - 1] = make_pair(d, cnt), ord[cnt] = v;
+    // rnk: preorder
+    in[v] = cnt;
+    cost[v] = c;
+    depth[cnt + N - 1] = make_pair(d, cnt);
+    ord[cnt] = v;
     ++cnt;
 
-    for (Edge e : g[v]) {
-      if (e.dst == p) continue;
-      dfs(e.dst, v, d + 1, c + e.cost);
-      depth[cnt + N - 1] = make_pair(d, cnt), ord[cnt] = v;
+    for (auto& [nv, nc] : g[v]) {
+      if (nv == p) continue;
+      dfs(nv, v, d + 1, c + nc);
+      depth[cnt + N - 1] = make_pair(d, cnt);
+      ord[cnt] = v;
       ++cnt;
     }
+
+    out[v] = cnt;
   }
 
-  pair<int, int> rmq(int a, int b, int i = 0, int l = 0, int r = -1) {
+  std::pair<int, int> get_subtree(int v, bool skip_root = false) {
+    return make_pair(in[v] + (int)skip_root, out[v]);
+  }
+
+  std::pair<int, int> rmq(int a, int b, int i = 0, int l = 0, int r = -1) {
     if (r == -1) r = N;
     if (r <= a || b <= l) return make_pair(1 << 30, 0);
     if (a <= l && r <= b) return depth[i];
@@ -52,7 +64,7 @@ struct EulerTour {
   }
 
   int lca(int x, int y) {
-    return ord[rmq(min(rnk[x], rnk[y]), max(rnk[x], rnk[y]) + 1).second];
+    return ord[rmq(min(in[x], in[y]), max(in[x], in[y]) + 1).second];
   }
   int dist(int x, int y) { return cost[x] + cost[y] - cost[lca(x, y)] * 2; }
 };
