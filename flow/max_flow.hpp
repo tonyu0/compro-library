218a44d9a81b468e2d202b15cd4a46ba9524490d
tonyu0 <tonyu.grapefruit@gmail.com>
2024-10-13 00:13:38 +0900

    [max_flow] cleanup

--- flow/max_flow.hpp
+++ flow/max_flow.hpp
@@ -2,14 +2,6 @@
 #include <queue>
 #include <vector>
 
-// 説明:
-// 最大流問題を解く。
-// 高速化ポイント:
-// 1. dist配列により残余グラフ上の最短経路
-// 2. start配列により探索した辺を管理することで同じ辺を何度も探索しない。
-// 3. 終点からdfsをすることで始点から終点に辿り着けないパスを探索しない。
-// 4. (高速化ではないが)DFSが一回で済むように工夫してある。
-
 // 動的木を用いるとdfsがO(VE) ->
 // O(ElogV)になるらしい。それで全体がO(VElogV)に改善される。
 
@@ -22,20 +14,20 @@
 //    各要素を2つの集合(選択)に分類する時、上手くグラフを作ると頂点を最適に始点側or終点側へと分離する問題、つまり最小カットに帰着できる。
 //    https://atcoder.jp/contests/arc085/submissions/58625726
 
-// Dinic O(|E||V|^2)
 template <typename T, bool directed = true>
-class Dinic {
+class max_flow {
   using Edge = std::tuple<int, size_t, T>;
 
 public:
-  Dinic(int n) : n(n), edges(n) {}
+  max_flow(int n) : n(n), edges(n) {}
   void add_edge(int from, int to, T cap) {
     // 逆に流し戻すために逆辺の情報を持つ必要があるが、隣接リストで持つために逆辺を示すインデックスをいい感じに持たせる
     edges[from].emplace_back(to, edges[to].size(), cap);
     edges[to].emplace_back(from, edges[from].size() - 1, directed ? 0 : cap);
   }
 
-  T max_flow(int s, int t) {
+  // Dinic O(|E||V|^2) -> bfs O(V), dfs O(VE)
+  T flow(int s, int t) {
     T flow = 0;
     while (bfs(s, t)) {
       start.assign(n, 0);
@@ -54,7 +46,6 @@ private:
   // dist: 残余グラフ上の始点からの各点の最短距離
   // start: 各点で探索した辺のインデックス、ステップごとに初期化
 
-  // O(V)
   // 残余グラフ上でsからの最短距離を計算する。tに辿り着けなかったらfalseを返す。
   bool bfs(int s, int t) {
     dist.assign(n, -1);
@@ -73,16 +64,14 @@ private:
     return dist[t] != -1;
   }
 
-  // 逆辺をたどる。
-  // limit: 現時点で流せる最大量
   // 1ステップでvから流せる量をresにまとめることで、DFSが一回で済む。
+  // 終点からdfsをすることで始点から終点に辿り着けないパスを探索しない。
   T dfs(int to, int s, T limit) {
     if (to == s) { return limit; }
     // s - ... - from - to - ... - t
 
     T res = 0;
-    // 無駄な辺を何度も調べないために、各頂点に既に調べた辺を持たせる。(start)
-    // 各頂点でdfsを続きからできるようにするイメージ
+    // 無駄な辺を何度も調べないために、各頂点に既に調べた辺を持たせる。各頂点でdfsを続きからできるようにするイメージ
     for (int &i = start[to]; i < static_cast<int>(edges[to].size()); ++i) {
       auto &[from, revidx, revcap] = edges[to][i];
       auto &[_to, _revidx, cap] = edges[from][revidx];
@@ -101,8 +90,8 @@ private:
     return res;
   }
 
-  // 参考: FordFulkerson
-  // 貪欲法+逆辺による押し戻しにより最大流問題を解くアルゴリズム O(max_flow * E)
+  // 参考: FordFulkerson O(max_flow * E)
+  // 貪欲法+逆辺による押し戻しにより最大流問題を解くアルゴリズム
   // std::vector<int> used;
   // int check = 1;
   // T dfs(int from, int t, T flow) {
@@ -124,7 +113,7 @@ private:
   //   }
   //   return 0;
   // }
-  // T max_flow(int s, int t) {
+  // T flow(int s, int t) {
   //   T flow = 0;
   //   while (true) {
   //     T f = dfs(s, t, std::numeric_limits<T>::max());
